83084fa28793357f7b4e4a2e86e98641f806c352
dvfleet413 <dvfleet413@gmail.com>
2020-06-17 09:06:29 -0400

    working on notes controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,39 @@
+class NotesController < ApplicationController
+    before_action :set_note, only: [:show, :update, :destory]
+
+    def index
+        @notes = Note.all
+        render json: @notes.to_json
+    end
+
+    def show
+        render json: @note.to_json
+    end
+
+    def create
+        note = Note.create!(notes_params)
+        if note.save
+            render json: note.to_json
+        else
+    end
+
+    def update
+        @note.update!(notes_params)
+        render json: @note.to_json
+    end
+
+    def destroy
+        @note.destroy
+        render json: Note.all.to_json
+    end
+
+    private
+        def notes_params
+            params.require(:note).permit(:author, :location, :content)
+        end
+
+        def set_note
+            @note = Note.find_by(id: params[:id])
+        end
+
+end
